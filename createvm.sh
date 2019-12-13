b60e8916d5f31332677434703c956446cce963ae
thuannv199 <58841358+thuannv199@users.noreply.github.com>
2019-12-13 15:58:27 +0700

    Update createvm.sh

--- createvm.sh
+++ createvm.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-KEY="SSH Key Name"
-BOOTIMG="IMAGE UUID"
-ZONE="NL1"
-FLAVOR="Standard 1"
-
-source ~/computerc 
-echo "Creating VM ${RUN}""
-VMUUID=$(nova boot \
+# KEY="SSH Key Name"
+BOOTIMG="9b2cb0e3-0394-4780-8caf-90096af8e523"  #image uuid
+ZONE="nova"
+FLAVOR="m1.small"
+#Create instance
+source ~/computerc #initiate user environment
+echo "Creating VM ${RUN}"
+VMUUID=$(openstack server create \
      --image "${BOOTIMG}" \
      --flavor "${FLAVOR}" \
      --availability-zone "${ZONE}" \
-     --nic net-id=00000000-0000-0000-0000-000000000000 \
-     --key-name "${KEY}" \
+     --nic net-id=30061cb1-6d10-4b79-ac9b-5047330354c4 \
      --user-data user_data.file \
      "VPS-${RUN}-${ZONE}" | awk '/id/ {print $4}' | head -n 1);
-
-until [[ "$(nova show ${VMUUID} | awk '/status/ {print $4}')" == "ACTIVE" ]]; do
-        :
-done
+until [[ "$(openstack server show ${VMUUID} | awk '/status/ {print $4}')" == "ACTIVE" ]]; do
 echo "VM ${RUN} (${VMUUID}) is active."
+done
+#Ouput
+CONSOLE_LINK=$(openstack console url show --spice ${VMUUID});
+echo "Console link in: ${CONSOLE_LINK}"
+echo "IP address: $(openstack server show ${VMUUID} | awk '/addresses/ {print $4}')"
