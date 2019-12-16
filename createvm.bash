68325c10a6ef14d63a3026c2ca232159052839cf
thuannv199 <58841358+thuannv199@users.noreply.github.com>
2019-12-16 10:07:37 +0700

    Update createvm.bash

--- createvm.bash
+++ createvm.bash
@@ -22,32 +22,32 @@ do
 done
 echo #New line
 #Verify
-read -p "Are you sure? " -n 1 -r
+read -p "Are you sure? " -r
 echo    #new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     export OS_PROJECT_NAME=$user_project
     export OS_USERNAME=$user_name
     export OS_PASSWORD=$char
+    # KEY="SSH Key Name"
+    BOOTIMG="9b2cb0e3-0394-4780-8caf-90096af8e523"  #image uuid
+    ZONE="nova"
+    FLAVOR="m1.small"
+    #Create instance
+    source ~/computerc #initiate user environment
+    echo "Creating VM ${RUN}"
+    VMUUID=$(openstack server create \
+        --image "${BOOTIMG}" \
+         --flavor "${FLAVOR}" \
+        --availability-zone "${ZONE}" \
+        --nic net-id=30061cb1-6d10-4b79-ac9b-5047330354c4 \
+        --user-data user_data.file \
+        "VPS-${RUN}-${ZONE}" | awk '/id/ {print $4}' | head -n 1);
+    until [[ "$(openstack server show ${VMUUID} | awk '/status/ {print $4}')" == "ACTIVE" ]]; do
+    echo "VM ${RUN} (${VMUUID}) is active."
+    done
+    #Ouput
+    CONSOLE_LINK=$(openstack console url show --spice ${VMUUID});
+    echo "Console link in: ${CONSOLE_LINK}"
+    echo "IP address: $(openstack server show ${VMUUID} | awk '/addresses/ {print $4}')"
 fi
-# KEY="SSH Key Name"
-BOOTIMG="9b2cb0e3-0394-4780-8caf-90096af8e523"  #image uuid
-ZONE="nova"
-FLAVOR="m1.small"
-#Create instance
-source ~/computerc #initiate user environment
-echo "Creating VM ${RUN}"
-VMUUID=$(openstack server create \
-     --image "${BOOTIMG}" \
-     --flavor "${FLAVOR}" \
-     --availability-zone "${ZONE}" \
-     --nic net-id=30061cb1-6d10-4b79-ac9b-5047330354c4 \
-     --user-data user_data.file \
-     "VPS-${RUN}-${ZONE}" | awk '/id/ {print $4}' | head -n 1);
-until [[ "$(openstack server show ${VMUUID} | awk '/status/ {print $4}')" == "ACTIVE" ]]; do
-echo "VM ${RUN} (${VMUUID}) is active."
-done
-#Ouput
-CONSOLE_LINK=$(openstack console url show --spice ${VMUUID});
-echo "Console link in: ${CONSOLE_LINK}"
-echo "IP address: $(openstack server show ${VMUUID} | awk '/addresses/ {print $4}')"
