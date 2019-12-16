d9c528cec165439ec6d297296b7166f4ade5d66f
thuannv199 <58841358+thuannv199@users.noreply.github.com>
2019-12-16 13:27:22 +0700

    Update createvm.bash

--- createvm.bash
+++ createvm.bash
@@ -34,17 +34,17 @@ then
     ZONE="nova"
     FLAVOR="m1.small"
     #Create instance
-    source ~/computerc #initiate user environment
-    echo "Creating VM ${RUN}"
+    #source ~/computerc #initiate user environment
+    echo "Creating VM ..."
     VMUUID=$(openstack server create \
         --image "${BOOTIMG}" \
          --flavor "${FLAVOR}" \
         --availability-zone "${ZONE}" \
         --nic net-id=30061cb1-6d10-4b79-ac9b-5047330354c4 \
         --user-data user_data.file \
-        "VPS-${RUN}-${ZONE}" | awk '/id/ {print $4}' | head -n 1);
+        "VPS-${ZONE}" | awk '/id/ {print $4}' | head -n 1);
     until [[ "$(openstack server show ${VMUUID} | awk '/status/ {print $4}')" == "ACTIVE" ]]; do
-    echo "VM ${RUN} (${VMUUID}) is active."
+    echo "VM (${VMUUID}) is active."
     done
     #Ouput
     CONSOLE_LINK=$(openstack console url show --spice ${VMUUID});
